c2dea3a5885eda9424db8e8845200cfa82804360
yoyoyojoe <jpma.family@gmail.com>
2022-08-28 08:33:07 -0700

    Add core functionality to calculator

--- src/utils/basic-calculator.js
+++ src/utils/basic-calculator.js
@@ -1,93 +1,66 @@
-let runningTotal = 0;
-let buffer = "0";
-let previousOperator;
-const screen = document.querySelector(".calculations");
+export default function (calc, setCalc) {
+  return {
+    handleAC: () => {
+      setCalc({ operator: "", num: 0, result: 0 });
+    },
+    handleButtonClick: (value) => {
+      const numberString = value.toString();
 
-export default {
-  runningTotal: 0,
-  buffer: "0",
-  previousOperator: undefined,
-  buttonClick: function buttonClick(value) {
-    console.log("value clicked: ", value);
-    if (isNaN(parseInt(value))) {
-      this.handleSymbol(value);
-    } else {
-      this.handleNumber(value);
-    }
-    this.rerender();
-  },
-
-  handleNumber: function handleNumber(value) {
-    if (this.buffer === "0") {
-      this.buffer = value;
-    } else {
-      this.buffer += value;
-    }
-  },
-
-  handleMath: function handleMath(value) {
-    if (this.buffer === "0") {
-      // do nothing
-      return;
-    }
-
-    const intBuffer = parseInt(this.buffer);
-    if (this.runningTotal === 0) {
-      this.runningTotal = intBuffer;
-    } else {
-      this.flushOperation(intBuffer);
-    }
-
-    previousOperator = value;
-
-    this.buffer = "0";
-  },
-
-  flushOperation: function flushOperation(intBuffer) {
-    if (previousOperator === "+") {
-      this.runningTotal += intBuffer;
-    } else if (previousOperator === "-") {
-      this.runningTotal -= intBuffer;
-    } else if (previousOperator === "×") {
-      this.runningTotal *= intBuffer;
-    } else {
-      this.runningTotal /= intBuffer;
-    }
-  },
-
-  handleSymbol: function handleSymbol(value) {
-    switch (value) {
-      case "AC":
-        this.buffer = "0";
-        this.runningTotal = 0;
-        break;
-      case "=":
-        if (previousOperator === null) {
-          // need two numbers to do math
-          return;
-        }
-        this.flushOperation(parseInt(this.buffer));
-        previousOperator = null;
-        this.buffer = +this.runningTotal;
-        this.runningTotal = 0;
-        break;
-      case "←":
-        if (this.buffer.length === 1) {
-          this.buffer = "0";
-        } else {
-          this.buffer = this.buffer.substring(0, this.buffer.length - 1);
-        }
-        break;
-      case "+":
-      case "-":
-      case "×":
-      case "÷":
-        this.handleMath(value);
-        break;
-    }
-  },
-
-  rerender: function rerender() {
-    screen.innerText = this.buffer;
-  },
-};
+      let numberValue;
+      if (numberString === "0" && calc.num === 0) {
+        numberValue = "0";
+      } else {
+        numberValue = Number(calc.num + numberString);
+      }
+      setCalc({
+        ...calc,
+        num: numberValue,
+      });
+    },
+    handleDecimal: (value) => {
+      setCalc({
+        ...calc,
+        num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
+      });
+    },
+    handleBasicOperators: (value) => {
+      setCalc({
+        operator: value,
+        result: !calc.result && calc.num ? calc.num : calc.result,
+        num: 0,
+      });
+    },
+    handleEquals: () => {
+      if (calc.result && calc.num) {
+        const math = (a, b, operator) => {
+          const result = {
+            "+": (a, b) => a + b,
+            "–": (a, b) => a - b,
+            "×": (a, b) => a * b,
+            "÷": (a, b) => a / b,
+          };
+          return result[operator](a, b);
+        };
+        setCalc({
+          result: math(calc.result, calc.num, calc.operator),
+          operator: "",
+          num: 0,
+        });
+      }
+    },
+    handlePercent: () => {
+      setCalc({
+        num: calc.num / 100,
+        result: calc.result / 100,
+        operator: "",
+      });
+    },
+    handleInvert: () => {
+      setCalc({
+        num: calc.num ? calc.num * -1 : 0,
+        result: calc.result ? calc.result * -1 : 0,
+        operator: "",
+      });
+    },
+  };
+}
